eb63685d9cd5a2fef329c0dc1f9138f5d743ff49
EffYou <EffYou@LAPTOP-I78H4JEP>
2023-10-30 22:55:38 +0800

    added unit test

--- tests/Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
+++ tests/Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
@@ -42,7 +42,8 @@ public class UpdateTodoItemDetailTests : BaseTestFixture
             Id = itemId,
             ListId = listId,
             Note = "A1",
-            Priority = PriorityLevel.High
+            Priority = PriorityLevel.High,
+            Tags = "development,unit,test"
         };
 
         await SendAsync(command);
@@ -52,6 +53,7 @@ public class UpdateTodoItemDetailTests : BaseTestFixture
         item.Should().NotBeNull();
         item!.ListId.Should().Be(command.ListId);
         item.Note.Should().Be(command.Note);
+        item.Tags.Should().Be(command.Tags);
         item.Priority.Should().Be(command.Priority);
         item.LastModifiedBy.Should().NotBeNull();
         item.LastModifiedBy.Should().Be(userId);
